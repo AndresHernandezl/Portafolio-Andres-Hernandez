2d453c71686b725cd070946f2117ce331f6c0c9a
Andres Hernandez <tronwares@gmail.com>
2015-05-19 11:03:52 -0500

    Bootstrap Min
    Subida de archivos

---
+++ Diseño Web/WorkShop/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <?php include 'inc/header-common.php'  ?>
+    <title>Workshop | Colombia Uniminuto</title>
+</head>
+<body>
+    
+    
+    <?php include 'inc/footer-common.php'  ?>
+</body>
+</html>
